d73921cbd00d4dc8c36d59e803dc3e21878fcd7f
suryaselvam1812 <suryaselvam1812@gmail.com>
2025-03-24 10:02:26 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+from typing import list
+HYPEN_E_DOT='-e .'
+def get_requirement(file_path:str)->list[str]:
+    '''this function will return the list of requirement '''
+    requirements=[]
+    with  open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n"," ")for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="surya",
+    author_email="suryaselvam1812gamil.com",
+    packages=find_packages(),
+    install_requires=get_requirement('requirement.txt')
+)
